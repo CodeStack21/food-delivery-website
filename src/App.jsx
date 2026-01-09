bd98870d674079743c802d28f131b83a12af67fe
yashasvi_patil_21 <yashasvipatil21@gmail.com>
2026-01-09 13:41:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import { Route, Routes } from 'react-router-dom'
 import Signup from './Components/Signup'
 import Home from './Components/Home'
 import Login from './Components/Login'
-// import Landing from './Components/Landing'
 import ProtectedRoute from './Components/ProtectedRoute'
 
 
 
 function App() {
   const user = JSON.parse(localStorage.getItem("user"));
-  // JSON.parse --> converts strings to JS obj.
 
   return (
     
