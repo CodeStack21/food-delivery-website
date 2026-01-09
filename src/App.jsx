b6afa5cdb2920d63247d4d97f67c304eb7bf6c2c
yashasvi_patil_21 <yashasvipatil21@gmail.com>
2026-01-09 13:42:38 +0530

    Clean up comments in App component
    Removed unnecessary comments from the App component.

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,13 @@ import Home from './Components/Home'
 import Login from './Components/Login'
 import ProtectedRoute from './Components/ProtectedRoute'
 
-
-
 function App() {
   const user = JSON.parse(localStorage.getItem("user"));
 
   return (
     
     <Routes>
-      {/* it is container->holds all routes */}
       <Route path="/" element={<Login/>}/>
-      {/* defines single route(path+Component) */}
       <Route path="/signup" element={<Signup/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/home" element={ <ProtectedRoute><Home/></ProtectedRoute>}/>
