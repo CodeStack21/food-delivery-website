32d317111c06a5f2f30ee2ed717decfde7e31f5c
yashasvi_patil_21 <yashasvipatil21@gmail.com>
2026-01-09 13:47:05 +0530

    Remove comments from ProtectedRoute component
    Removed comments explaining the ProtectedRoute component functionality.

--- src/Components/ProtectedRoute.jsx
+++ src/Components/ProtectedRoute.jsx
@@ -2,8 +2,6 @@ import { Navigate } from "react-router-dom";
 
 
 const ProtectedRoute=({children}) =>{
-    // ProtectedRoute checks login status and either shows the gage or redirects to login
-    // children means --> whatever component is wrapped inside ProtectedRoute
     const user= JSON.parse(localStorage.getItem("user"));
     if(!user){
         return <Navigate to ="/login"/>;
